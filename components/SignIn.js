d5664a6acae73061d1af4b6c168aa62f3fa901a7
deep-69bits <deepakchattwaniking15@gmail.com>
2023-03-24 14:13:18 +0530

    changes

--- components/SignIn.js
+++ components/SignIn.js
@@ -95,7 +95,7 @@ const SignIn = () => {
                     onChange={changepassword}
                   />
                   <button
-                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-[200px] m-auto py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                   >
                     <svg
                       className="w-6 h-6 -ml-2"
